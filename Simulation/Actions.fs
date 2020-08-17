5c612f827c7ba1f77dda1bdde3255450e35f3cb8
Luke Kaalim <lkaalim@nine.com.au>
2020-08-17 20:35:52 +1000

    Correctly indent actions

--- Simulation/Actions.fs
+++ Simulation/Actions.fs
@@ -1,17 +1,18 @@
 ﻿module Actions
-  type SpawnMecha = {
-    time: int;
-    mecha: Mecha.T;
-  }
-  type DriveMecha = {
-    time: int;
-    id: Mecha.ID;
-    velocity: Vector2D.T;
-  }
 
-  type T =
-    | SpawnMecha of SpawnMecha
-    | DriveMecha of DriveMecha
-    
-  let SpawnMecha (time, mecha) = SpawnMecha({ time = time; mecha = mecha })
-  let DriveMecha (time, id, velocity) = DriveMecha({ time = time; id = id; velocity = velocity })
+type SpawnMecha = {
+  time: int;
+  mecha: Mecha.T;
+}
+type DriveMecha = {
+  time: int;
+  id: Mecha.ID;
+  velocity: Vector2D.T;
+}
+
+type T =
+  | SpawnMecha of SpawnMecha
+  | DriveMecha of DriveMecha
+  
+let SpawnMecha (time, mecha) = SpawnMecha({ time = time; mecha = mecha })
+let DriveMecha (time, id, velocity) = DriveMecha({ time = time; id = id; velocity = velocity })
