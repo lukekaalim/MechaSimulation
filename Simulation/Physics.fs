e83485b8318c42178ea5aa1c9c54c88b4abc357d
Luke Kaalim <lukekaalim@gmail.com>
2020-08-29 15:46:21 +1000

    fix incorrect collision result

--- Simulation/Physics.fs
+++ Simulation/Physics.fs
@@ -1,28 +1,27 @@
 module Physics
 
-type Particle2D = {
-    initialPosition: Vector2D.T;
-    velocity: Vector2D.T;
-    radius: int;
-  }
+type Particle2D =
+  { initialPosition: Vector2D.T
+    velocity: Vector2D.T
+    radius: int }
 
-type Particle1D = {
-  position: int;
-  velocity: int;
-  radius: int;
-}
+type Particle1D =
+  { position: int
+    velocity: int
+    radius: int }
 
-type Particle =
-  | Particle1D of {| position: int; velocity: int; radius: int |}
+type Particle = Particle1D of {| position: int
+                                 velocity: int
+                                 radius: int |}
 
 type Collision1DResult =
   | Miss
   | Hit of entry: int * exit: int
 
 let GetCollision1D particleA particleB =
-  let offset = particleA.position - particleB.position
+  let offset = particleA.position + particleB.position
   let velocity = particleA.velocity - particleB.velocity
   let collisonDistance = particleA.radius + particleB.radius
   match velocity with
-    | 0 -> Miss
-    | _ -> Hit(entry = (offset - collisonDistance) / velocity, exit = (offset + collisonDistance) / velocity)
+  | 0 -> Miss
+  | _ -> Hit(entry = (offset - collisonDistance) / velocity, exit = (offset + collisonDistance) / velocity)
